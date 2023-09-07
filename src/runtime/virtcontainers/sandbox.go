9c0e23c34632bb3ae29125aebc068b4508cd5248
Alexandru Matei <alexandru.matei@uipath.com>
2023-09-07 11:50:01 +0300

    fix for trying to add a device to cgroup
    In case the path points to a regular file an error is generated
    
    Fixes: #7859
    
    Signed-off-by: Alexandru Matei <alexandru.matei@uipath.com>

--- src/runtime/virtcontainers/sandbox.go
+++ src/runtime/virtcontainers/sandbox.go
@@ -2190,9 +2190,11 @@ func (s *Sandbox) HotplugAddDevice(ctx context.Context, device api.Device, devTy
 	defer span.End()
 
 	if s.sandboxController != nil {
-		if err := s.sandboxController.AddDevice(device.GetHostPath()); err != nil {
-			s.Logger().WithError(err).WithField("device", device).
-				Warnf("Could not add device to the %s controller", s.sandboxController)
+		if major, _ := device.GetMajorMinor(); major != -1 {
+			if err := s.sandboxController.AddDevice(device.GetHostPath()); err != nil {
+				s.Logger().WithError(err).WithField("device", device).
+					Warnf("Could not add device to the %s controller", s.sandboxController)
+			}
 		}
 	}
 
@@ -2243,9 +2245,11 @@ func (s *Sandbox) HotplugAddDevice(ctx context.Context, device api.Device, devTy
 func (s *Sandbox) HotplugRemoveDevice(ctx context.Context, device api.Device, devType config.DeviceType) error {
 	defer func() {
 		if s.sandboxController != nil {
-			if err := s.sandboxController.RemoveDevice(device.GetHostPath()); err != nil {
-				s.Logger().WithError(err).WithField("device", device).
-					Warnf("Could not add device to the %s controller", s.sandboxController)
+			if major, _ := device.GetMajorMinor(); major != -1 {
+				if err := s.sandboxController.RemoveDevice(device.GetHostPath()); err != nil {
+					s.Logger().WithError(err).WithField("device", device).
+						Warnf("Could not add device to the %s controller", s.sandboxController)
+				}
 			}
 		}
 	}()
